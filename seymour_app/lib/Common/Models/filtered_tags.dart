e9783a82a4834af1613d9da98c10ea700590b427
rspacerr <definitelynotoberon@gmail.com>
2024-07-27 01:16:27 -0400

    WIP fetch with filtering

---
+++ seymour_app/lib/Common/Models/filtered_tags.dart
@@ -0,0 +1,19 @@
+class FilteredTags {
+  static final List<String> tags = [
+    'Sculpture',
+    'Statue',
+    'Mural',
+    'Graffiti',
+    'Installation',
+    'Bust',
+    'Stone',
+    'Painting',
+    'Architecture',
+    'Mosaic',
+    'Relief',
+    'Fountain',
+    'Street Art',
+    'Azulejo',
+    'Land Art',
+  ];
+}
--- seymour_app/lib/Common/Queries/sights_by_radius.dart
+++ seymour_app/lib/Common/Queries/sights_by_radius.dart
@@ -2,6 +2,7 @@ import 'package:flutter_overpass/flutter_overpass.dart';
 // ignore: depend_on_referenced_packages
 import 'package:latlong2/latlong.dart';
 import 'package:seymour_app/Common/Models/coordinate.dart';
+import 'package:seymour_app/Common/Models/filtered_tags.dart';
 import 'dart:async';
 import 'dart:developer'; // for log()
 
@@ -9,6 +10,23 @@ import 'package:seymour_app/Common/Models/sight.dart';
 import 'package:seymour_app/Common/Queries/get_filters.dart';
 
 final flutterOverpass = FlutterOverpass();
+
+Future<bool> matchesFilters(dynamic node) async {
+  if (node["tags"] == null || 
+      node["tags"]["tourism"] == null || 
+      node["tags"]["name"] == null) {
+        return false;
+  }
+
+  List<bool> filters = await getFilters();
+  for (int i = 0; i < filters.length; i++) {
+    if (!filters[i] && node["tags"][FilteredTags.tags[i]]) {
+      return false;
+    }
+  }
+  return true;
+}
+
 
 Future<List<Sight>> getSights(LatLng center, double radius) async {
   final queryResult = await flutterOverpass.rawOverpassQL(
@@ -20,9 +38,7 @@ Future<List<Sight>> getSights(LatLng center, double radius) async {
   List<Sight> nearby = [];
 
   for (dynamic node in queryResult["elements"]) {
-    if (node["tags"] != null &&
-        node["tags"]["tourism"] != null &&
-        node["tags"]["name"] != null) {
+    if (await matchesFilters(node)) {
       nearby.add(Sight(
           node["tags"]["name"],
           Coordinate(node["lat"], node["lon"]),

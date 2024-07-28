6f53e46dbaf25453a7af19fb3507ceee449ac36e
Darmin-Geek <34427428+Darmin-Geek@users.noreply.github.com>
2024-07-28 16:03:27 -0400

    Added stop audio guide button

--- seymour_app/lib/Views/navigation_page.dart
+++ seymour_app/lib/Views/navigation_page.dart
@@ -3,6 +3,7 @@ import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:flutter_map/flutter_map.dart';
 import 'package:flutter_popup/flutter_popup.dart';
+import 'package:flutter_tts/flutter_tts.dart';
 import 'package:http/http.dart';
 import 'package:latlong2/latlong.dart';
 import 'package:location/location.dart';
@@ -116,126 +117,127 @@ class _NavigationPageState extends State<NavigationPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: SlidingUpPanel(
-            isDraggable: false,
-            panelSnapping: true,
-            controller: _pc,
-            body: FutureBuilder<LocationData?>(
-                future: currentLocation(),
-                builder:
-                    (BuildContext context, AsyncSnapshot<dynamic> snapchat) {
-                  if (snapchat.hasData) {
-                    final LocationData currentLocation = snapchat.data;
-                    return FlutterMap(
-                        options: MapOptions(
-                          initialCenter: currentJourney
-                              .route!.legs.first.points.first
-                              .toLatLng(),
-                          initialZoom: 17,
-                        ),
-                        children: [
-                          TileLayer(
-                            urlTemplate:
-                                'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
-                          ),
-                          PolylineLayer(polylines: [...routeLines]),
-                          CircleLayer(circles: [userLocationMarker]),
-                          MarkerLayer(
-                            markers: currentJourney
-                                .sights()
-                                .map((Sight sight) => Marker(
-                                    point: sight.getCoordinate().toLatLng(),
-                                    child: CustomPopup(
-                                        child: Icon(
-                                          Icons.location_pin,
-                                          size: 30,
-                                          color: Colors.red,
-                                        ),
-                                        content: SizedBox(
-                                            width: 200,
-                                            height: 200,
-                                            child: Column(
-                                                mainAxisAlignment:
-                                                    MainAxisAlignment.start,
-                                                // mainAxisAlignment:
-                                                //     // MainAxisAlignment.spaceBetween,
-                                                children: <Widget>[
-                                                  Text(sight.name(),
-                                                      textScaler:
-                                                          TextScaler.linear(
-                                                              1.2)),
-                                                  ElevatedButton(
-                                                      child: const Text(
-                                                          "Open in Wikipedia"),
-                                                      onPressed:
-                                                          sight.getWikipediaTitle() ==
-                                                                  null
-                                                              ? null
-                                                              : () async {
-                                                                  await launchUrl(
-                                                                      Uri.https(
-                                                                          "en.wikipedia.org",
-                                                                          "/wiki/${sight.getWikipediaTitle()}"),
-                                                                      mode: LaunchMode
-                                                                          .inAppBrowserView);
-                                                                }),
-                                                  ElevatedButton(
-                                                      onPressed:
-                                                          sight.getWikipediaTitle() ==
-                                                                  null
-                                                              ? null
-                                                              : () {
-                                                                  readWikipediaArticleAloud(
-                                                                      sight
-                                                                          .getWikipediaTitle()!);
-                                                                },
-                                                      child: const Text(
-                                                          "Read wikipedia audio guide aloud"))
-                                                ])))))
-                                .toList(),
-                          ),
-                        ]);
-                  }
-                  return const Center(child: CircularProgressIndicator());
-                }),
-            panel: Container(
-                color: Colors.grey,
-                child: Column(children: <Widget>[
-                  const Text("YOU'RE HERE", textScaler: TextScaler.linear(2)),
-                  ElevatedButton(
-                      child: const Text("Raise/lower demo"),
-                      onPressed: () {
-                        _toggleDestinationMenu();
-                      })
-                ])),
-            // These are the navigation instructions
-            // TODO: Global button that calls _showDestinationMenu
-            collapsed: Container(
-                color: Colors.white,
-                child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: <Widget>[
-                      Icon(
-                        maneuverIcons[currentInstruction.maneuver] ??
-                            Icons.error,
-                        size: 40,
+      body: SlidingUpPanel(
+          isDraggable: false,
+          panelSnapping: true,
+          controller: _pc,
+          body: FutureBuilder<LocationData?>(
+              future: currentLocation(),
+              builder: (BuildContext context, AsyncSnapshot<dynamic> snapchat) {
+                if (snapchat.hasData) {
+                  final LocationData currentLocation = snapchat.data;
+                  return FlutterMap(
+                      options: MapOptions(
+                        initialCenter: currentJourney
+                            .route!.legs.first.points.first
+                            .toLatLng(),
+                        initialZoom: 17,
                       ),
-                      Column(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: <Widget>[
-                            Text(
-                                formatInstructionMessage(
-                                        currentInstruction.street) ??
-                                    "",
-                                textScaler: TextScaler.linear(1.2)),
+                      children: [
+                        TileLayer(
+                          urlTemplate:
+                              'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
+                        ),
+                        PolylineLayer(polylines: [...routeLines]),
+                        CircleLayer(circles: [userLocationMarker]),
+                        MarkerLayer(
+                          markers: currentJourney
+                              .sights()
+                              .map((Sight sight) => Marker(
+                                  point: sight.getCoordinate().toLatLng(),
+                                  child: CustomPopup(
+                                      child: Icon(
+                                        Icons.location_pin,
+                                        size: 30,
+                                        color: Colors.red,
+                                      ),
+                                      content: SizedBox(
+                                          width: 200,
+                                          height: 200,
+                                          child: Column(
+                                              mainAxisAlignment:
+                                                  MainAxisAlignment.start,
+                                              // mainAxisAlignment:
+                                              //     // MainAxisAlignment.spaceBetween,
+                                              children: <Widget>[
+                                                Text(sight.name(),
+                                                    textScaler:
+                                                        TextScaler.linear(1.2)),
+                                                ElevatedButton(
+                                                    child: const Text(
+                                                        "Open in Wikipedia"),
+                                                    onPressed:
+                                                        sight.getWikipediaTitle() ==
+                                                                null
+                                                            ? null
+                                                            : () async {
+                                                                await launchUrl(
+                                                                    Uri.https(
+                                                                        "en.wikipedia.org",
+                                                                        "/wiki/${sight.getWikipediaTitle()}"),
+                                                                    mode: LaunchMode
+                                                                        .inAppBrowserView);
+                                                              }),
+                                                ElevatedButton(
+                                                    onPressed:
+                                                        sight.getWikipediaTitle() ==
+                                                                null
+                                                            ? null
+                                                            : () {
+                                                                readWikipediaArticleAloud(
+                                                                    sight
+                                                                        .getWikipediaTitle()!);
+                                                              },
+                                                    child: const Text(
+                                                        "Read wikipedia audio guide aloud"))
+                                              ])))))
+                              .toList(),
+                        ),
+                      ]);
+                }
+                return const Center(child: CircularProgressIndicator());
+              }),
+          panel: Container(
+              color: Colors.grey,
+              child: Column(children: <Widget>[
+                const Text("YOU'RE HERE", textScaler: TextScaler.linear(2)),
+                ElevatedButton(
+                    child: const Text("Raise/lower demo"),
+                    onPressed: () {
+                      _toggleDestinationMenu();
+                    })
+              ])),
+          // These are the navigation instructions
+          // TODO: Global button that calls _showDestinationMenu
+          collapsed: Container(
+              color: Colors.white,
+              child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: <Widget>[
+                    Icon(
+                      maneuverIcons[currentInstruction.maneuver] ?? Icons.error,
+                      size: 40,
+                    ),
+                    Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: <Widget>[
+                          Text(
+                              formatInstructionMessage(
+                                      currentInstruction.street) ??
+                                  "",
+                              textScaler: TextScaler.linear(1.2)),
 
-                            // Text("Example Dr.", textScaler: TextScaler.linear(1.5))
-                          ]),
-                      ElevatedButton(
-                          child: const Text("Go back"),
-                          onPressed: () {
-                            navigateToMapPage();
-                          })
-                    ]))));
+                          // Text("Example Dr.", textScaler: TextScaler.linear(1.5))
+                        ]),
+                    ElevatedButton(
+                        child: const Text("Go back"),
+                        onPressed: () {
+                          navigateToMapPage();
+                        })
+                  ]))),
+      floatingActionButton:
+          ElevatedButton(child: Icon(Icons.stop), onPressed: FlutterTts().stop),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+    );
   }
 }

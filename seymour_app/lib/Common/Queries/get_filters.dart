9221ea3de74f53363767193c655d344d5b54f463
rspacerr <definitelynotoberon@gmail.com>
2024-07-29 12:45:00 -0400

    removed debug logs and commented out code

--- seymour_app/lib/Common/Queries/get_filters.dart
+++ seymour_app/lib/Common/Queries/get_filters.dart
@@ -4,10 +4,9 @@ import 'dart:async';
 import 'package:path_provider/path_provider.dart';
 import 'dart:io';
 import 'dart:convert';
-import 'dart:developer';
 
 import 'package:seymour_app/Common/Models/filtered_tags.dart';
-import 'package:seymour_app/Common/Models/tag.dart'; // for log()
+import 'package:seymour_app/Common/Models/tag.dart'; 
 
 Future<File> get _localFile async {
   final directory = await getApplicationDocumentsDirectory();
@@ -23,40 +22,11 @@ Future<Map<String, Tag>> setFiltersInternal() async {
     return FilteredTags.tags;
   }
 
-  log("Before: ");
-  log(FilteredTags.tags.entries.toString());
-
   int i = 0;
   for (var entry in FilteredTags.tags.entries) {
     entry.value.filtered = jsonObject[i++]["filtered"];
   }
 
-  log("After: ");
-  log(FilteredTags.tags.entries.toString());
-
   return FilteredTags.tags;
-
-  /*
-  final file = await _localFile;
-  String filtersRaw = await file.readAsString();
-  String stringFilters = filtersRaw.substring(1, filtersRaw.length-1);
-  List<String> fsplit = stringFilters.split(',');
-
-  log(fsplit.toString());
-
-  List<bool> filters = [];
-  for (String s in fsplit) {
-    filters.add(s == "true");
-  }
-
-  log(filters.toString());
-
-  int i = 0;
-  for (var value in FilteredTags.tags.values) {
-    value.filtered = filters[i++];
-  }
-
-  log(filters.toString());
-  */
 
 }
